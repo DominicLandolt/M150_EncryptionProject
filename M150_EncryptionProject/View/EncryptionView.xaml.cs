968cbbcc1bd6611d5fa1e689a2dea2f803a601fb
Dominic Landolt <dominiclandolt@swissonline.ch>
2021-08-27 10:21:13 +0200

    Created MVVM structure

---
+++ M150_EncryptionProject/View/EncryptionView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using M150_EncryptionProject.ViewModel;
+using System.Windows;
+
+namespace M150_EncryptionProject.View
+{
+    /// <summary>
+    /// Interaction logic for EncryptionView.xaml
+    /// </summary>
+    public partial class EncryptionView : Window
+    {
+        public EncryptionView()
+        {
+            DataContext = new EncryptionViewModel(this);
+            InitializeComponent();
+        }
+    }
+}
---
+++ M150_EncryptionProject/ViewModel/EncryptionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using M150_EncryptionProject.View;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace M150_EncryptionProject.ViewModel
+{
+    public class EncryptionViewModel : INotifyPropertyChanged
+    {
+        EncryptionView _view;
+
+        public EncryptionViewModel(EncryptionView view)
+        {
+            _view = view;
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
+    }
+}
